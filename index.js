56213d9b94c9f40325829b2a7a2ddd3274f09d46
rkatsala <rkatsala@gmail.com>
2015-07-23 19:12:26 +0300

    Merge testing to master

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 function randomRange(min, max) {
     min = min || 1;
     max = max || 100;
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 console.log(randomRange(1, 20));
-console.log(randomRange());for (var i = 0; i < 10; i++) {
+console.log(randomRange());
+
+for (var i = 0; i < 10; i++) {
     console.log(randomRange(1, 20));
 }
