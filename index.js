23f273f18192f89d7acc59ae2d932514fab75b17
rkatsala <rkatsala@gmail.com>
2015-07-23 18:51:23 +0300

    Added min, max values

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 function randomRange(min, max) {
+    min = min || 1;
+    max = max || 100;
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 console.log(randomRange(1, 20));
+console.log(randomRange());
